cbc7a171d41f539d03fca719abdad30919bd57d6
Luz Gutierrez <mili01gm@gmail.com>
2017-05-10 00:53:02 -0500

    avance js del scroll, detalles css para estilos de titulos.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-/*
- * Archivo principal de JS
- */
+window.addEventListener("scroll",function(){
+  var nav = document.getElementById("nav");
+  var child = document.getElementById("nav-title");
+  var scrollMenu = window.pageYOffset || document.body.scrollTop;
+  if(scrollMenu>20){
+    nav.classList.add("after-scroll");
+    child.classList.add("h2-scroll");
+    // console.log(child);
+  } else {
+    nav.classList.remove("after-scroll");
+    child.classList.remove("h2-scroll");
+  }
+});
