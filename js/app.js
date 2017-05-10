37bde56e92e12abc12659f09f98dc868ab0ba0a5
Luz Gutierrez <mili01gm@gmail.com>
2017-05-10 11:44:59 -0500

    Portfolio, done. About, done.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
+//For NAV resize on scroll
 window.addEventListener("scroll",function(){
   var nav = document.getElementById("nav");
   var child = document.getElementById("nav-title");
   var scrollMenu = window.pageYOffset || document.body.scrollTop;
-  if(scrollMenu>20){
+  if(scrollMenu>50){
     nav.classList.add("after-scroll");
     child.classList.add("h2-scroll");
-    // console.log(child);
+
   } else {
     nav.classList.remove("after-scroll");
     child.classList.remove("h2-scroll");
